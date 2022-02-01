f85aab1a77a850cdf1993d263fab5776bcb991ae
Genevieve Flynn <g.a.flynn19@gmail.com>
2022-02-01 17:59:35 -0500

    Add login page kind of.

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import PropTypes from 'prop-types';
+
+import "../assets/login.css"
+
+async function loginUser(credentials) {
+
+    const url = 'http://localhost:8081/login'
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(credentials)
+    }
+
+    console.log(JSON.stringify(credentials))
+
+    return fetch(url, options)
+        .then(data => data.json())
+}
+
+function Login({ setToken }) {
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+
+        const token = await loginUser({
+            username,
+            password
+        });
+
+        setToken(token);
+    }
+
+    return (
+        <div className="login-wrapper">
+            <form onSubmit={handleSubmit}>
+                <h1> Login </h1>
+                <label><p>username </p><input type="text" onChange={e => setUsername(e.target.value)} placeholder="username"></input></label>
+                <label><p>password </p><input type="password" onChange={e => setPassword(e.target.value)} placeholder="password"></input></label>
+                <div>
+                    <button type="submit">Login</button>
+                </div>
+                {/* <span>Forgot <a href="">password</a></span> */}
+            </form>
+        </div>
+    );
+}
+
+Login.propTypes = {
+    setToken: PropTypes.func.isRequired
+}
+
+export default Login;
--- client/src/index.js
+++ client/src/index.js
@@ -1,38 +1,3 @@
-/*import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import {
-  Navigation,
-  Footer,
-  Home,
-  About,
-  NewContract,
-  Contracts,
-  Settings,
-} from "./components";
-
-ReactDOM.render(
-  <Router>
-    <Navigation />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/newContract" element={<NewContract />} />
-      <Route path="/contracts" element={<Contracts />} />
-      <Route path="/settings" element={<Settings />} />
-    </Routes>
-    <Footer />
-  </Router>,
-
-  document.getElementById("root")
-);
-
-serviceWorker.unregister();*/
-
-
-   
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -45,38 +10,3 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
-/*
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import {
-  Navigation,
-  Footer,
-  Home,
-  About,
-  NewContract,
-  Contracts,
-  Settings,
-} from "./components";
-
-ReactDOM.render(
-  <Router>
-    <Navigation />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/newContract" element={<NewContract />} />
-      <Route path="/contracts" element={<Contracts />} />
-      <Route path="/settings" element={<Settings />} />
-    </Routes>
-    <Footer />
-  </Router>,
-
-  document.getElementById("root")
-);
-
-serviceWorker.unregister();
-*/
