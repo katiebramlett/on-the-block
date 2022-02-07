1c232967053bd4808e3da8402d0867fcc39bb91c
Genevieve Flynn <g.a.flynn19@gmail.com>
2022-02-07 13:45:33 -0500

    Fix create account styling.

--- client/src/components/CreateAccount.jsx
+++ client/src/components/CreateAccount.jsx
@@ -1,6 +1,10 @@
 import React, {useState} from "react";
 import { axiosBackend  } from "../utils/axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSignInAlt } from '@fortawesome/free-solid-svg-icons';
+
+
+import '../assets/createaccount.css'
 
 
 function CreateAccount() {
@@ -19,14 +23,14 @@ function CreateAccount() {
         <div className="create-account-wrapper">
             <form onSubmit={createAccount}>
                 <h1>Create an Account</h1>
-                <label><p>username </p><input type="text" onChange={e => setUsername(e.target.value)} placeholder="username"></input></label>
-                <label><p>password </p><input type="password" onChange={e => setPassword(e.target.value)} placeholder="password"></input></label>
-                <label><p>confirm password </p><input type="password" onChange={e => setPassword(e.target.value)} placeholder="confirm password"></input></label>
+                <label><p>username </p><input type="text" onChange={e => setUsername(e.target.value)} placeholder="username"></input></label><br></br>
+                <label><p>password </p><input type="password" onChange={e => setPassword(e.target.value)} placeholder="password"></input></label><br></br>
+                <label><p>confirm password </p><input type="password" onChange={e => setPassword(e.target.value)} placeholder="confirm password"></input></label><br></br>
                 <div>
                     <button type="submit">SIGN UP</button>
                 </div>
                 <div>
-                    <p>Already a user? <br></br> <a href="/login"> Login</a></p>
+                    <p>Already a user? <br></br> <a href="/login"> Login <FontAwesomeIcon icon={faSignInAlt}/></a></p>
                 </div>
             </form>
         </div>
