4a3dfa4594ae874a39304406aa40eca5a804aabe
Genevieve Flynn <g.a.flynn19@gmail.com>
2022-02-07 17:12:10 -0500

    Add error messages to create account page.

--- client/README.md
+++ client/README.md
@@ -1,4 +1,4 @@
-## Tutorials Found Helpful
+## React Tutorials to Revisit 
 
 - [App Login Tutorial](https://www.digitalocean.com/community/tutorials/how-to-add-login-authentication-to-react-applications#introduction)
 
@@ -7,3 +7,5 @@
 - [React Routing](https://blog.logrocket.com/react-router-dom-tutorial-examples/)
 
 - [React Redirecting](https://www.pluralsight.com/guides/how-to-set-react-router-default-route-redirect-to-home)
+
+- [Private Page Rerouting](https://dev.to/nilanth/how-to-create-public-and-private-routes-using-react-router-72m#:~:text=The%20private%20route%20component%20is,is%20authenticated%20(Logged%20in).)
--- client/src/components/CreateAccount.jsx
+++ client/src/components/CreateAccount.jsx
@@ -2,12 +2,13 @@ import React, {useState} from "react";
 import { axiosBackend  } from "../utils/axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSignInAlt } from '@fortawesome/free-solid-svg-icons';
+import PropTypes from 'prop-types';
 
 
 import '../assets/createaccount.css'
 
 
-function CreateAccount() {
+function CreateAccount({ setToken }) {
 
     const [username, setUsername] = useState();
     const [password, setPassword] = useState();
@@ -16,7 +17,18 @@ function CreateAccount() {
 
     const createAccount = async e => {
 
+        e.preventDefault()
 
+        if (!username) setError("Username is a required field."); 
+        else if (!password) setError("Password is required field.");
+        else if (!confirmPassword) setError("Confirm password is a required field.")
+        else if (password != confirmPassword) setError("Passwords must match.") 
+
+        // all input is correct, attempt to create a new user 
+        else {
+            setToken("test123");
+            window.location.href = '/'   
+        }
     }
 
     return (
@@ -25,7 +37,10 @@ function CreateAccount() {
                 <h1>Create an Account</h1>
                 <label><p>username </p><input type="text" onChange={e => setUsername(e.target.value)} placeholder="username"></input></label><br></br>
                 <label><p>password </p><input type="password" onChange={e => setPassword(e.target.value)} placeholder="password"></input></label><br></br>
-                <label><p>confirm password </p><input type="password" onChange={e => setPassword(e.target.value)} placeholder="confirm password"></input></label><br></br>
+                <label><p>confirm password </p><input type="password" onChange={e => setconfirmPassword(e.target.value)} placeholder="confirm password"></input></label><br></br>
+                {
+                    error && <div style={{color: 'white'}}>{error}</div>
+                }
                 <div>
                     <button type="submit">SIGN UP</button>
                 </div>
@@ -35,7 +50,12 @@ function CreateAccount() {
             </form>
         </div>
     )
-
 }
+
+/* Proptypes check for if the system data matched expected types during runtime */
+CreateAccount.propTypes = {
+    setToken: PropTypes.func.isRequired
+}
+
 
 export default CreateAccount;
