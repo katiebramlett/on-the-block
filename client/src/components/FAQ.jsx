47c7fb53ff77852f5e2cce84296a1c9a19564882
Katie Bramlett <kbramlett@gwu.edu>
2022-03-23 11:21:11 -0400

    FAQ web scraping template

--- client/src/components/FAQ.jsx
+++ client/src/components/FAQ.jsx
@@ -6,6 +6,36 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
 
 function FAQ() {
+
+    /* WEB SCRAPING
+    const fetch = require("node-fetch");
+    const cheerio = require("cheerio");
+    const fs = require("fs");
+    // function to get the raw data
+    const getRawData = (URL) => {
+        return fetch(URL)
+            .then((response) => response.text())
+            .then((data) => {
+                return data;
+            });
+    };
+    // URL for data
+    const URL = "https://example.com/";
+    // start of the program
+    const scrapeData = async () => {
+    const rawData = await getRawData(URL);
+    // parsing the data
+    const parsedData = cheerio.load(rawData);
+        console.log(parsedData);
+        // write code to extract the data
+        // here
+        // ...
+        // ...
+    };
+    // invoking the main function
+    scrapeData();
+    */
+
     return (
         <div className="faq">
             <div className="container">
